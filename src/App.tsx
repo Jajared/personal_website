2f058df4a630cc29739ced7dbf6f0a845ac8c551
Jajared <jajawong09@gmail.com>
2023-07-27 14:45:07 +0800

    Fixed exceed screen width.

--- src/App.tsx
+++ src/App.tsx
@@ -50,80 +50,82 @@ const App: React.FC = () => {
         <div className="flex flex-col mt-16">
           <p className="font-bold underline">Tech stack</p>
           <div className="justify-center block mt-2 ">
-            <ul className="flex flex-row mt-2 space-x-4">
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={ReactIcon} title="react-icon" alt="react-icon" className="w-8 h-8"></img>
+            <div className="overflow-x-auto">
+              <ul className="flex flex-row mt-2 space-x-4">
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={ReactIcon} title="react-icon" alt="react-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">React</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">React</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={FlutterIcon} title="flutter-icon" alt="flutter-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={FlutterIcon} title="flutter-icon" alt="flutter-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Flutter</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Flutter</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={TypescriptIcon} title="typescript-icon" alt="typescript-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={TypescriptIcon} title="typescript-icon" alt="typescript-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Typescript</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Typescript</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={JavascriptIcon} title="js-icon" alt="js-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={JavascriptIcon} title="js-icon" alt="js-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Javascript</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Javascript</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={FirebaseIcon} title="firebase-icon" alt="firebase-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={FirebaseIcon} title="firebase-icon" alt="firebase-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Firebase</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Firebase</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={CSSIcon} title="css-icon" alt="css-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={CSSIcon} title="css-icon" alt="css-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">CSS</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">CSS</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={HTMLIcon} title="html-icon" alt="html-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={HTMLIcon} title="html-icon" alt="html-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">HTML</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">HTML</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={PythonIcon} title="python-icon" alt="python-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={PythonIcon} title="python-icon" alt="python-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Python</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Python</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center justify-center w-8 h-8 group">
-                  <div>
-                    <img src={JavaIcon} title="java-icon" alt="java-icon" className="w-8 h-8"></img>
+                </li>
+                <li>
+                  <div className="flex items-center justify-center w-8 h-8 group">
+                    <div>
+                      <img src={JavaIcon} title="java-icon" alt="java-icon" className="w-8 h-8"></img>
+                    </div>
+                    <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Java</div>
                   </div>
-                  <div className="absolute mt-[72px] opacity-0 text-sm font-medium text-gray-500 transition-opacity duration-150 group-hover:opacity-100">Java</div>
-                </div>
-              </li>
-            </ul>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
